4ce4adb635ca862f17eb019bdfe1187418c94582
johngray1206 <johngray1206@gmail.com>
2023-04-03 19:25:44 -0400

    upload jar
    Former-commit-id: 3ec74ae46fdd36554b39ca9ed966124d4c12d3e0

--- src/test/kotlin/io/gray/LightTheLampApplicationTests.kt
+++ src/test/kotlin/io/gray/LightTheLampApplicationTests.kt
@@ -1,9 +1,9 @@
 package io.gray
 
+import io.micronaut.test.extensions.junit5.annotation.MicronautTest
 import org.junit.jupiter.api.Test
-import org.springframework.boot.test.context.SpringBootTest
 
-@SpringBootTest
+@MicronautTest(transactional = false)
 class LightTheLampApplicationTests {
 
 	@Test
