6f28e129f6c64a2d73bb95174ff43e5440856160
johngray1206 <johngray1206@gmail.com>
2023-04-06 00:15:44 -0400

    add registration, work with new domain, try to get confirmation stuff working
    Former-commit-id: 653ceb2261478c0f603824b77341e0dc616bc3bc

--- frontend/login.js
+++ frontend/login.js
@@ -3,12 +3,61 @@ if (jwt != null) {
   window.location.href = "./index.html";
 }
 
+document.getElementById("register").onclick = function () {
+    location.href = "./register.html";
+};
+
+function GetURLParameter(sParam)
+{
+    var sPageURL = window.location.search.substring(1);
+    var sURLVariables = sPageURL.split('&');
+    for (var i = 0; i < sURLVariables.length; i++)
+    {
+        var sParameterName = sURLVariables[i].split('=');
+        if (sParameterName[0] == sParam)
+        {
+            return sParameterName[1];
+        }
+    }
+}
+
+if(GetURLParameter("confirmation")) {
+    const xhttp = new XMLHttpRequest();
+      xhttp.open("GET", "https://www.lightthelamp.dev/api/confirm/" + GetURLParameter("confirmation"));
+      xhttp.setRequestHeader("Content-Type", "application/json;charset=UTF-8");
+      xhttp.send();
+      xhttp.onreadystatechange = function () {
+        if (this.readyState == 4) {
+          const user = JSON.parse(this.responseText);
+          console.log(objects);
+          if (user["confirmed"] == true) {
+            Swal.fire({
+              text: "Account Confirmed! Please login.",
+              icon: "success",
+              confirmButtonText: "OK",
+            }).then((result) => {
+              if (result.isConfirmed) {
+                window.location.href = "./login.html";
+              }
+            });
+          } else {
+            Swal.fire({
+              text: user["message"],
+              icon: "error",
+              confirmButtonText: "OK",
+            });
+          }
+        }
+      };
+      return false;
+}
+
 function login() {
   const username = document.getElementById("username").value;
   const password = document.getElementById("password").value;
 
   const xhttp = new XMLHttpRequest();
-  xhttp.open("POST", "http://157.230.209.115/api/login");
+  xhttp.open("POST", "https://www.lightthelamp.dev/api/login");
   xhttp.setRequestHeader("Content-Type", "application/json;charset=UTF-8");
   xhttp.send(
     JSON.stringify({
