23a36d21eb36894e3bf99525f9e896264d8d566b
johngray1206 <johngray1206@gmail.com>
2023-04-10 19:37:49 -0400

    pull pick to the top?

--- frontend/index.js
+++ frontend/index.js
@@ -97,7 +97,7 @@ function createTable(game, picks, user, activeGame, sortedGames) {
     caption.setAttribute("class","caption-top");
 
     var nonGoalies = game.players.filter(player => player.position != "Goalie")
-        .sort((a, b) => (a.name.split(" ").reverse().join(",") > b.name.split(" ").reverse().join(",")) ? 1 : -1);
+        .sort((a, b) => (a.name.split(" ").reverse().join(",") > b.name.split(" ").reverse().join(",")) ? 1 : -1)
         .sort((a, b) => ((pick && pick.gamePlayer && pick.gamePlayer.id.playerId == a.id.playerId) ? 1 : -1);
 
     for(var i = 0; i < nonGoalies.length; i++) {
