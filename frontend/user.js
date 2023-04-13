1b7a0d26dd994db2fdc4ea374c4bb49391632c1c
johngray1206 <johngray1206@gmail.com>
2023-04-13 13:10:13 -0400

    implement user editing menu pt23

--- frontend/user.js
+++ frontend/user.js
@@ -23,36 +23,37 @@ function getUser() {
     };
 };
 
-function updateUser() {
-    var formData = new FormData();
-    formData.append('file', document.getElementById('profilePic').files[0]);
-    formData.append('displayName', document.getElementById('displayName').value);
-    const xhttp = new XMLHttpRequest();
-    xhttp.open("PUT", "/api/user");
-    //xhttp.setRequestHeader("Content-Type", "multipart/form-data");
-    xhttp.setRequestHeader("Authorization", "Bearer " + jwt);
-    xhttp.send(formData);
-    xhttp.onreadystatechange = function () {
-      if (this.readyState == 4) {
-          if (this.status == 200 || this.status == 204) {
-            const objects = JSON.parse(this.responseText);
-            console.log(objects);
-            Swal.fire({
-              text: "User update successful!",
-              icon: "success",
-              confirmButtonText: "OK",
-            }).then((result) => {
-              if (result.isConfirmed) {
-                window.location.href = "./user.html";
-              }
-            });
-          } else if (this.status == 401 || this.status == 403) {
-             localStorage.removeItem("jwt");
-             window.location.href = "./login.html";
-         }
-     }
-    };
-    return false;
-};
+document.forms.userUpdate.addEventListener('submit', e => {
+  e.preventDefault();
+
+  let formData = new FormData(e.target);
+  formData.set('profilePic', document.getElementById('profilePic').files[0]);
+
+  const xhttp = new XMLHttpRequest();
+      xhttp.open("PUT", "/api/user");
+      xhttp.setRequestHeader("Authorization", "Bearer " + jwt);
+      xhttp.send(formData);
+      xhttp.onreadystatechange = function () {
+        if (this.readyState == 4) {
+            if (this.status == 200 || this.status == 204) {
+              const objects = JSON.parse(this.responseText);
+              console.log(objects);
+              Swal.fire({
+                text: "User update successful!",
+                icon: "success",
+                confirmButtonText: "OK",
+              }).then((result) => {
+                if (result.isConfirmed) {
+                  window.location.href = "./user.html";
+                }
+              });
+            } else if (this.status == 401 || this.status == 403) {
+               localStorage.removeItem("jwt");
+               window.location.href = "./login.html";
+           }
+       }
+      };
+      return false;
+});
 
 getUser();
