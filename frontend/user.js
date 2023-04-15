c713ae21c5e354d12d7085c68545e502df00df67
johngray1206 <johngray1206@gmail.com>
2023-04-15 02:13:53 -0400

    fix frontend for these new changes pt21

--- frontend/user.js
+++ frontend/user.js
@@ -1,36 +1,53 @@
 function getUser() {
-    const xhttp = new XMLHttpRequest();
-    xhttp.open("GET", "/api/user");
-    xhttp.setRequestHeader("Content-Type", "application/json;charset=UTF-8");
-    xhttp.setRequestHeader("Authorization", "Bearer " + jwt);
-    xhttp.send();
-    xhttp.onreadystatechange = function () {
+
+    const xhttp1 = new XMLHttpRequest();
+    xhttp1.open("GET", "/api/teams");
+    xhttp1.setRequestHeader("Content-Type", "application/json;charset=UTF-8");
+    xhttp1.setRequestHeader("Authorization", "Bearer " + jwt);
+    xhttp1.send();
+    xhttp1.onreadystatechange = function () {
       if (this.readyState == 4) {
           if (this.status == 200) {
-            const user = JSON.parse(this.responseText);
-            console.log(user);
-            if (user.displayName) {
-                document.getElementById("displayName").value = user.displayName;
-            }
-            if (user.profilePic) {
-                document.getElementById("profilePicPreview").src = "data:image/png;base64," + user.profilePic;
-            }
-            var index = 0;
-            user.teams.forEach((team) => {
-                var option = document.createElement("option");
-                option.value = team.id;
-                option.innerHTML = team.teamName;
-                var teamSelect = document.getElementById("team");
-                teamSelect.append(option);
-                teamSelect.options[index].selected = true;
-                index++;
-            });
-          } else if (this.status == 401 || this.status == 403) {
-             localStorage.removeItem("jwt");
-             window.location.href = "./login.html";
-         }
-     }
-    };
+            const teams = JSON.parse(this.responseText);
+            teams.sort((a, b) => a.teamName.localeCompare(b.teamName));
+            console.log(teams);
+            const xhttp = new XMLHttpRequest();
+            xhttp.open("GET", "/api/user");
+            xhttp.setRequestHeader("Content-Type", "application/json;charset=UTF-8");
+            xhttp.setRequestHeader("Authorization", "Bearer " + jwt);
+            xhttp.send();
+            xhttp.onreadystatechange = function () {
+              if (this.readyState == 4) {
+                  if (this.status == 200) {
+                    const user = JSON.parse(this.responseText);
+                    console.log(user);
+                    if (user.displayName) {
+                        document.getElementById("displayName").value = user.displayName;
+                    }
+                    if (user.profilePic) {
+                        document.getElementById("profilePicPreview").src = "data:image/png;base64," + user.profilePic;
+                    }
+                    var index = 0;
+                    teams.forEach((team) => {
+                        var option = document.createElement("option");
+                        option.value = team.id;
+                        option.innerHTML = team.teamName;
+                        var teamSelect = document.getElementById("team");
+                        teamSelect.append(option);
+                        if (user.teams.some((userTeam) => team.id == userTeam.id)) {
+                            teamSelect.options[index].selected = true;
+                        }
+                        index++;
+                    });
+                  } else if (this.status == 401 || this.status == 403) {
+                     localStorage.removeItem("jwt");
+                     window.location.href = "./login.html";
+                 }
+             }
+            };
+        }
+    }
+    }
 };
 
 document.forms.userUpdate.addEventListener('submit', e => {
