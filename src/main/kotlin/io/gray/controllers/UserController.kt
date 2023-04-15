69dd49d553913f9eb468325c4756a4b9e40a2f30
johngray1206 <johngray1206@gmail.com>
2023-04-15 02:40:51 -0400

    fix frontend for these new changes pt22

--- src/main/kotlin/io/gray/controllers/UserController.kt
+++ src/main/kotlin/io/gray/controllers/UserController.kt
@@ -5,7 +5,9 @@ import io.gray.model.UserRequest
 import io.gray.email.MailService
 import io.gray.model.Team
 import io.gray.model.User
+import io.gray.model.UserTeam
 import io.gray.repos.UserRepository
+import io.gray.repos.UserTeamRepository
 import io.micronaut.http.HttpRequest
 import io.micronaut.http.MediaType
 import io.micronaut.http.annotation.*
@@ -13,6 +15,7 @@ import io.micronaut.http.server.util.DefaultHttpClientAddressResolver
 import io.micronaut.security.annotation.Secured
 import io.micronaut.security.rules.SecurityRule
 import org.mindrot.jbcrypt.BCrypt
+import reactor.core.publisher.Flux
 import reactor.core.publisher.Mono
 import java.lang.IllegalStateException
 import java.security.Principal
@@ -28,6 +31,7 @@ import kotlin.apply
 open class UserController(
         private val userRepository: UserRepository,
         private val httpClientAddressResolver: DefaultHttpClientAddressResolver,
+        private val userTeamRepository: UserTeamRepository,
         private val mailService: MailService
 ) {
 
@@ -58,10 +62,22 @@ open class UserController(
     }
 
     @Put(consumes = [MediaType.MULTIPART_FORM_DATA])
-    fun update(profilePic: ByteArray?, displayName: String?, principal: Principal): Mono<User> {
+    fun update(profilePic: ByteArray?, displayName: String?, teams: List<Long>?, principal: Principal): Mono<User> {
         return userRepository.findByEmail(principal.name)
-                .flatMap {
-                    userRepository.update(it.apply {
+                .flatMap { user ->
+                    teams?.let { teams ->
+                        user.teams?.let { userTeams ->
+                            Flux.fromIterable(userTeams).filter { it.id !in teams }
+                                    .flatMap { userTeamRepository.findByUserIdAndTeamId(user.id!!, it.id!!) }
+                                    .map { userTeamRepository.delete(it) }.thenMany(
+                                            Flux.fromIterable(teams).filter { it !in userTeams.mapNotNull { userTeam -> userTeam.id } }
+                                                    .map { UserTeam().apply { this.userId = user.id; this.teamId = it; } }
+                                                    .flatMap { userTeamRepository.save(it) }
+                                    ).then(Mono.just(user))
+                        } ?: Mono.just(user)
+                    } ?: Mono.just(user)
+                }.flatMap { user ->
+                    userRepository.update(user.apply {
                         profilePic?.let { this.profilePic = it }
                         displayName?.let { this.displayName = it }
                     })
