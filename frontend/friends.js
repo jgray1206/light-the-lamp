4bc481fded1f4ab1ca9164dd24e32ee65665f3f4
johngray1206 <johngray1206@gmail.com>
2023-05-02 22:15:43 -0400

    add friends page pt3
    Former-commit-id: 2d1b847ef305c64af262215596f6b5f1c3f49974

--- frontend/friends.js
+++ frontend/friends.js
@@ -9,33 +9,34 @@ function loadFriends() {
           if (this.status == 200) {
               var user = JSON.parse(this.responseText);
               console.log(user);
-              if (user.friends.length == 0) {
-                document.getElementById("card-body").append("You don't have any friends yet! Send your link to your friends and have them click it.");
+              if (user.friends == undefined || user.friends.length == 0) {
+                  document.getElementById("card-body").append("You don't have any friends yet! Send your link to your friends and have them click it.");
+              } else {
+                  tbody = document.getElementById("friends-table");
+                  user.friends.forEach(function (friend) {
+                       tr = document.createElement("tr");
+                       picTd = document.createElement("td");
+                       picTdImd = document.createElement("imd");
+                       picTdImg.src = "data:image/png;base64," + friend.profilePic;
+                       picTd.appendChild(picTdImg);
+                       tr.appendChild(picTd);
+
+                       nameTd = document.createElement("td");
+                       nameTd.innerHTML = friend.displayName;
+                       tr.appendChild(nameTd);
+
+                       removeButtonTd = document.createElement("td");
+                       removeButton = document.createElement("button");
+                       removeButton.type = "button";
+                       removeButton.className = "btn btn-danger";
+                       removeButton.innerHtml = "Remove";
+                       removeButton.onclick = "removeFriend(\""+friend.confirmationUuid+"\");";
+
+                       tr.appendChild(removeButton);
+
+                       tbody.appendChild(tr);
+                  });
               }
-              tbody = document.getElementById("friends-table");
-              user.friends.forEach(function (friend) {
-                   tr = document.createElement("tr");
-                   picTd = document.createElement("td");
-                   picTdImd = document.createElement("imd");
-                   picTdImg.src = "data:image/png;base64," + friend.profilePic;
-                   picTd.appendChild(picTdImg);
-                   tr.appendChild(picTd);
-
-                   nameTd = document.createElement("td");
-                   nameTd.innerHTML = friend.displayName;
-                   tr.appendChild(nameTd);
-
-                   removeButtonTd = document.createElement("td");
-                   removeButton = document.createElement("button");
-                   removeButton.type = "button";
-                   removeButton.className = "btn btn-danger";
-                   removeButton.innerHtml = "Remove";
-                   removeButton.onclick = "removeFriend(\""+friend.confirmationUuid+"\");";
-
-                   tr.appendChild(removeButton);
-
-                   tbody.appendChild(tr);
-              });
           } else if (this.status == 401 || this.status == 403) {
               localStorage.removeItem("jwt");
               window.location.href = "./login.html";
