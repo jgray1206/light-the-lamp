9b0238786fcb6ae25fa62cd400cc78a04ba94cac
johngray1206 <johngray1206@gmail.com>
2023-05-03 00:29:02 -0400

    add friends page pt23
    Former-commit-id: 2fa774f87e65fa194b2bbfcd2b0be364f2067af3

--- frontend/login.js
+++ frontend/login.js
@@ -7,6 +7,20 @@ document.getElementById("register").onclick = function () {
     location.href = "./register.html";
 };
 
+function getURLParameter(sParam)
+{
+    var sPageURL = window.location.search.substring(1);
+    var sURLVariables = sPageURL.split('&');
+    for (var i = 0; i < sURLVariables.length; i++)
+    {
+        var sParameterName = sURLVariables[i].split('=');
+        if (sParameterName[0] == sParam)
+        {
+            return sParameterName[1];
+        }
+    }
+}
+
 if (getURLParameter("confirmation")) {
     const xhttp = new XMLHttpRequest();
     xhttp.open("GET", "/api/user/confirm/" + getURLParameter("confirmation"));
