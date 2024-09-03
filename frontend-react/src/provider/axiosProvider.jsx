2a2b14986b1ea832fed26589dfd2b76f1d776859
John Gray <johngray1206@gmail.com>
2024-09-03 16:59:35 -0400

    no www. in root api url

--- frontend-react/src/provider/axiosProvider.jsx
+++ frontend-react/src/provider/axiosProvider.jsx
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const AxiosInstance = axios.create({
-    baseURL: "https://www.lightthelamp.dev",
+    baseURL: "https://lightthelamp.dev",
     timeout: 10000
 });
 export default AxiosInstance;
