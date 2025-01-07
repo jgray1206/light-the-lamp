30f32a916a4d9ad895313fdf231d2633ecd9e9d9
jgray1206 <johngray1206@gmail.com>
2025-01-07 01:09:11 -0500

    ah put this script in a better non-cachey spot

--- frontend-react/src/App.jsx
+++ frontend-react/src/App.jsx
@@ -4,11 +4,19 @@ import Routes from "./components/Routes"
 import AuthProvider from './provider/authProvider';
 
 export default function App() {
-  return (
-    <div>
-      <AuthProvider>
-        <Routes />
-      </AuthProvider>
-    </div>
-  );
+    const storedTheme = localStorage.getItem('theme');
+    let theme;
+    if (storedTheme) {
+        theme = storedTheme;
+    } else {
+        theme = window.matchMedia('(prefers-color-scheme: dark)').matches ? 'dark' : 'light';
+    }
+    document.documentElement.setAttribute('data-bs-theme', theme);
+    return (
+        <div>
+            <AuthProvider>
+                <Routes/>
+            </AuthProvider>
+        </div>
+    );
 }
