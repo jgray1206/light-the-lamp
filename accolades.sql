214ad161359264c878def4255fdd6dd3a6d08f4b
John Gray <johngray1206@gmail.com>
2025-04-19 01:24:25 -0400

    more script fixes

--- accolades.sql
+++ accolades.sql
@@ -1,7 +1,7 @@
 --num picks
 select count(*) from public.pick p where team_id = 17 and game_id > '2024020000';
 --num users
-select count(*) from public.pick p where team_id = 17 and game_id > '2024020000' group by user_id;
+select count(*) from (select count(*) from public.pick p where team_id = 17 and game_id > '2024020000' group by user_id);
 
 --most points, top 3
 select points, user_id, reddit_username, display_name
@@ -69,14 +69,14 @@ order by p.count desc limit 3;
 select count(p), name
 from public.pick p
 join game_player gp ON p.game_player_id_player_id = gp.id_player_id and p.game_player_id_game_id = gp.id_game_id
-where p.team_id = 17 and game_id > '2024020000' and the_team is null and goalies is null and reddit_username IS NOT NULL
+where p.team_id = 17 and game_id > '2024020000' and the_team is null and goalies is null
 group by name order by p.count desc limit 3;
 
 --most picked red wing
 select count(p), name
 from public.pick p
 join game_player gp ON p.game_player_id_player_id = gp.id_player_id and p.game_player_id_game_id = gp.id_game_id
-where p.team_id = 17 and game_id > '2024020000' and the_team is null and goalies is null and reddit_username IS NOT NULL
+where p.team_id = 17 and game_id > '2024020000' and the_team is null and goalies is null
 group by name order by p.count asc limit 3;
 
 --perfect attendance
@@ -86,10 +86,10 @@ JOIN (
 select user_id, count(*) from public.pick p where team_id = 17 and game_id > '2024020000' group by user_id
 having count(*) = 82
 ) as p
-where reddit_username IS NOT NULL
-on u.id = p.user_id;
+on u.id = p.user_id
+where reddit_username IS NOT NULL;
 
 --num picks by team
 select count(*), MAX(team_name) from public.pick p
-JOIN public.team ON team_id = id
-where game_id > '2024020000' group by team_id;
+JOIN public.team t ON p.team_id = t.id
+where game_id > '2024020000' group by p.team_id;
