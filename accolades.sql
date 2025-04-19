c49230412806f5d38b60043ca324f44f5a4e9602
John Gray <johngray1206@gmail.com>
2025-04-19 01:17:58 -0400

    reddit only

--- accolades.sql
+++ accolades.sql
@@ -9,6 +9,7 @@ select sum(points) as points, user_id from public.pick p where team_id = 17 and 
 order by sum(points) desc
 ) as p
 on u.id = p.user_id
+where reddit_username IS NOT NULL
 order by points desc limit 6;
 
 -- best player picker (most points from the players)
@@ -19,6 +20,7 @@ select sum(points) as points, user_id from public.pick p where team_id = 17 and 
 and the_team is null group by user_id order by sum(points) desc
 ) as p
 on u.id = p.user_id
+where reddit_username IS NOT NULL
 order by points desc limit 6;
 
 --most consistent player picker
@@ -32,6 +34,7 @@ where p.team_id = 17 and game_id > '2024020000' and the_team is null and goalies
 group by user_id, name
 ) as p
 on u.id = p.user_id
+where reddit_username IS NOT NULL
 order by p.count desc limit 6;
 
 --most consistent team picker
@@ -44,6 +47,7 @@ where p.team_id = 17 and game_id > '2024020000' and the_team = True
 group by user_id
 ) as p
 on u.id = p.user_id
+where reddit_username IS NOT NULL
 order by p.count desc limit 6;
 
 --most consistent goalies picker
@@ -56,20 +60,21 @@ where p.team_id = 17 and game_id > '2024020000' and goalies = True
 group by user_id
 ) as p
 on u.id = p.user_id
+where reddit_username IS NOT NULL
 order by p.count desc limit 3;
 
 --least picked red wing
 select count(p), name
 from public.pick p
 join game_player gp ON p.game_player_id_player_id = gp.id_player_id and p.game_player_id_game_id = gp.id_game_id
-where p.team_id = 17 and game_id > '2024020000' and the_team is null and goalies is null
+where p.team_id = 17 and game_id > '2024020000' and the_team is null and goalies is null and reddit_username IS NOT NULL
 group by name order by p.count desc limit 3;
 
 --most picked red wing
 select count(p), name
 from public.pick p
 join game_player gp ON p.game_player_id_player_id = gp.id_player_id and p.game_player_id_game_id = gp.id_game_id
-where p.team_id = 17 and game_id > '2024020000' and the_team is null and goalies is null
+where p.team_id = 17 and game_id > '2024020000' and the_team is null and goalies is null and reddit_username IS NOT NULL
 group by name order by p.count asc limit 3;
 
 --perfect attendance
@@ -79,4 +84,5 @@ JOIN (
 select user_id, count(*) from public.pick p where team_id = 17 and game_id > '2024020000' group by user_id
 having count(*) = 82
 ) as p
+where reddit_username IS NOT NULL
 on u.id = p.user_id;
